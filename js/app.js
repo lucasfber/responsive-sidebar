4fd3fd3ee60d04c394bd9241c24805c02e785665
Lucas Bertoldo <lucas.fber@gmail.com>
2020-03-20 22:45:54 -0300

    Added IIFE to js file

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-const btnMenu = document.querySelector(".btn-menu")
-const btnClose = document.querySelector(".btn-close")
-const sidebar = document.querySelector(".sidebar")
-const btnWrapper = document.querySelector(".btn-wrapper")
+;(function() {
+  const btnMenu = document.querySelector(".btn-menu")
+  const btnClose = document.querySelector(".btn-close")
+  const sidebar = document.querySelector(".sidebar")
+  const btnWrapper = document.querySelector(".btn-wrapper")
 
-btnClose.addEventListener("click", () => {
-  btnClose.classList.toggle("hide")
-  btnMenu.classList.toggle("hide")
-  sidebar.classList.toggle("open")
-  btnWrapper.classList.toggle("transparent")
-})
+  btnClose.addEventListener("click", () => {
+    btnClose.classList.toggle("hide")
+    btnMenu.classList.toggle("hide")
+    sidebar.classList.toggle("open")
+    btnWrapper.classList.toggle("transparent")
+  })
 
-btnMenu.addEventListener("click", () => {
-  btnMenu.classList.toggle("hide")
-  btnClose.classList.toggle("hide")
-  sidebar.classList.toggle("open")
-  btnWrapper.classList.toggle("transparent")
-})
+  btnMenu.addEventListener("click", () => {
+    btnMenu.classList.toggle("hide")
+    btnClose.classList.toggle("hide")
+    sidebar.classList.toggle("open")
+    btnWrapper.classList.toggle("transparent")
+  })
+})()
